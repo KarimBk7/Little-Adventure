6123012631667ee76954596e01b1411f146145ee
Abdil Karim Bakir <abdilkarimb@gmail.com>
2022-11-16 11:15:32 +0100

    Kaiju Adventure v0.4
    Build Path geändert und Properties angepasst.

--- Kaiju Adventure/src/main/Main.java
+++ Kaiju Adventure/src/main/Main.java
@@ -7,8 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 	
 
-		JFrame window = new JFrame()
-;
+		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);				//Vom nutzer verstellbar (nein)
 		window.setTitle("Kaiju Adventure");	
