f3efe5e62a58050c37aa685260aad0c5318a6b74
Abdil Karim Bakir <abdilkarimb@gmail.com>
2022-12-07 11:09:47 +0100

    Kaiju Adventure v2.4.4
    Angler .png geändert
    Dialog geändert

--- Kaiju Adventure/src/main/ObjektSetter.java
+++ Kaiju Adventure/src/main/ObjektSetter.java
@@ -161,7 +161,7 @@ public class ObjektSetter {
 		gl.npc[3].dialogIndex = 7;
 		gl.npc[3].name = "haendler";
 		
-		gl.npc[1].image = sTool.setupImage("npc", "angler", gl.unitsize, gl.unitsize);
+		gl.npc[1].image = sTool.setupImage("npc", "siad", gl.unitsize, gl.unitsize);
 		gl.npc[2].image = sTool.setupImage("npc", "momo", gl.unitsize, gl.unitsize);
 		gl.npc[3].image = sTool.setupImage("npc", "seitensifir", gl.unitsize, gl.unitsize);
 	}
--- Kaiju Adventure/src/unit/Npc.java
+++ Kaiju Adventure/src/unit/Npc.java
@@ -23,7 +23,8 @@ public class Npc extends Unit{
 					+ "Erwarte aber nicht zu viel.";
 		
 		//Angler
-		dialog[2] = "Nachdem ich einen Fisch geangelt hab hol ich \nmir ein paar Aepfel vom Wald.";
+		dialog[2] = "Nachdem ich einen Fisch geangelt hab hol ich \nmir ein paar Aepfel vom Wald für meinen \n"
+					+ "Doppel-Apfel-Tabak.";
 		dialog[3] = "Hab gehoert das es welche etwas noerdlich von hier gibt.";
 		
 		//momo
